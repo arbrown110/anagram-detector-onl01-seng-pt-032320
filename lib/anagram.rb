f21465d0b6628ea851652695a90718c90801ea8e
chic-caller-7520 <arbrown110@gmail.com>
2020-04-16 03:57:18 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,15 +1,1 @@
-class Anagram
-  
-  attr_accessor :word 
-  
-  def initialize (word)
-    @word = word
-  end
-  
-  def match(array)
-    array.select do |word|
-    word.split("").sort == @name.split("")
-    end
-  end  
-end
 
