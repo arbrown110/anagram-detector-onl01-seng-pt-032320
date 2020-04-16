d4d6bb304500c7e3b1d23a442c5de6824649e97a
chic-caller-7520 <arbrown110@gmail.com>
2020-04-16 03:49:24 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -12,3 +12,16 @@ class Anagram
   end  
 end
 
+class Anagram
+
+  attr_accessor :name
+
+  def initialize(word)
+    @name = word 
+  end   
+
+  def match(array)
+    array.select {|x| x.split("").sort == @name.split("").sort}
+  end   
+
+end 
